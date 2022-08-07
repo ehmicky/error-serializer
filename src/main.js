565d4eafe782df10701925da437d1dd00f491812
ehmicky <ehmicky@gmail.com>
2022-08-07 21:48:21 +0200

    Add safe-json-value

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import normalizeException from 'normalize-exception'
+import safeJsonValue from 'safe-json-value'
 
 // We apply `normalize-exception` to ensure a strict input
 export const serialize = function (error) {
   const errorA = normalizeException(error)
   const object = errorA
-  return object
+  const objectA = safeJsonValue(object)
+  return objectA
 }
 
 // We apply `normalize-exception` to ensure a strict output
