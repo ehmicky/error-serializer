6baec1b98b632bb1d4e0c31d61d17c93b5861ccb
ehmicky <ehmicky@gmail.com>
2022-08-07 21:48:15 +0200

    Add initial implementation

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-export default function templateName(value) {
-  return value
+import normalizeException from 'normalize-exception'
+
+// We apply `normalize-exception` to ensure a strict input
+export const serialize = function (error) {
+  const errorA = normalizeException(error)
+  const object = errorA
+  return object
+}
+
+// We apply `normalize-exception` to ensure a strict output
+export const parse = function (object) {
+  const error = object
+  const errorA = normalizeException(error)
+  return errorA
 }
