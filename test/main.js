79ee527cbec32d0e022c2bf46d046faeaac4c1cd
ehmicky <ehmicky@gmail.com>
2022-08-14 23:20:21 +0200

    More tests

--- test/main.js
+++ test/main.js
@@ -1,6 +1,26 @@
 import test from 'ava'
 import { serialize } from 'error-serializer'
+import { each } from 'test-each'
 
-test('Serializes error name', (t) => {
-  t.is(serialize(new Error('test')).name, 'Error')
+const normalError = new Error('test')
+// eslint-disable-next-line fp/no-mutation
+normalError.cause = new Error('inner')
+// eslint-disable-next-line fp/no-mutation
+normalError.errors = [new Error('otherInner')]
+
+each(['name', 'message', 'stack'], ({ title }, propName) => {
+  test(`Serializes error core property | ${title}`, (t) => {
+    t.is(serialize(normalError)[propName], normalError[propName])
+  })
+
+  test(`Serializes error cause | ${title}`, (t) => {
+    t.is(serialize(normalError).cause[propName], normalError.cause[propName])
+  })
+
+  test(`Serializes aggregate errors | ${title}`, (t) => {
+    t.is(
+      serialize(normalError).errors[0][propName],
+      normalError.errors[0][propName],
+    )
+  })
 })
