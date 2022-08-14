7239ea54ea4bee7876ce34cfe2c1cc488792fb02
ehmicky <ehmicky@gmail.com>
2022-08-14 23:43:33 +0200

    Add types

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,7 +1,48 @@
 /**
- * `error-serializer` options
+ * Any JSON value.
  */
-export interface Options {}
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue }
+
+/**
+ *
+ */
+export interface ErrorObject {
+  /**
+   *
+   */
+  name: string
+
+  /**
+   *
+   */
+  message: string
+
+  /**
+   *
+   */
+  stack: string
+
+  /**
+   *
+   */
+  cause?: ErrorObject
+
+  /**
+   *
+   */
+  errors?: ErrorObject[]
+
+  /**
+   *
+   */
+  [key: PropertyKey]: JSONValue
+}
 
 /**
  *
@@ -9,4 +50,21 @@ export interface Options {}
  * ```js
  * ```
  */
-export default function errorSerializer(value: any, options?: Options): object
+export function serialize(error: unknown): ErrorObject
+
+/**
+ * `error-serializer` `parse()` options
+ */
+export interface ParseOptions {
+  readonly types?: {
+    [ErrorType: string]: typeof Error
+  }
+}
+
+/**
+ *
+ * @example
+ * ```js
+ * ```
+ */
+export function parse(errorObject: unknown, options?: ParseOptions): Error
