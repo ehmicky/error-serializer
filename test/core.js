bd022c7fa1beb1cea24a5b23403d73178c868794
ehmicky <ehmicky@gmail.com>
2022-08-14 23:22:03 +0200

    Add more tests

---
+++ test/core.js
@@ -0,0 +1,39 @@
+import test from 'ava'
+import { serialize } from 'error-serializer'
+
+test('Keep non-core properties', (t) => {
+  const error = new Error('test')
+  error.prop = true
+  t.is(serialize(error).prop, error.prop)
+})
+
+test('Does not serialize non-core properties recursively', (t) => {
+  const error = new Error('test')
+  error.one = { two: true, three: new Error('inner') }
+  t.deepEqual(serialize(error).one, { two: error.one.two, three: {} })
+})
+
+test('Ignore non-enumerable properties', (t) => {
+  const error = new Error('test')
+  // eslint-disable-next-line fp/no-mutating-methods
+  Object.defineProperty(error, 'prop', {
+    value: true,
+    enumerable: false,
+    writable: true,
+    configurable: true,
+  })
+  t.is(serialize(error).prop)
+})
+
+test('Ignore inherited properties', (t) => {
+  class CustomError extends Error {}
+  CustomError.prototype.prop = true
+  t.is(serialize(new CustomError('test')).prop)
+})
+
+test('Keep symbol properties', (t) => {
+  const error = new Error('test')
+  const symbol = Symbol()
+  error[symbol] = true
+  t.is(serialize(error)[symbol])
+})
