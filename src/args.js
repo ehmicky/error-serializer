1a2be4c32c175bb02162e46399011143092a98d4
ehmicky <ehmicky@gmail.com>
2022-09-25 15:47:12 +0200

    Add more tests

--- src/args.js
+++ src/args.js
@@ -1,19 +1,23 @@
 // `error.constructorArgs` can be set to define specific arguments to call
 // when instantiating the error during parsing.
 export const setConstructorArgs = function ({ constructorArgs }) {
-  if (Object.prototype.toString.call(constructorArgs) === '[object Arguments') {
-    return ['constructorArgs', [...constructorArgs]]
+  if (
+    Object.prototype.toString.call(constructorArgs) === '[object Arguments]'
+  ) {
+    return [['constructorArgs', [...constructorArgs]]]
   }
 
   if (Array.isArray(constructorArgs)) {
-    return ['constructorArgs', constructorArgs]
+    return [['constructorArgs', constructorArgs]]
   }
 
   return []
 }
 
 // Constructor arguments default to the error message and an empty object
-// as second argument
+// as second argument.
+// Static properties are still set after initialization, so `constructorArgs`
+// is only useful with custom `classes`.
 export const getConstructorArgs = function (
   constructorArgs,
   message,
---
+++ test/args.js
@@ -0,0 +1,78 @@
+import test from 'ava'
+import { serialize, parse } from 'error-serializer'
+
+import { SIMPLE_ERROR_OBJECT } from './helpers/main.js'
+
+test('constructorArgs can be set', (t) => {
+  const error = new Error('test')
+  error.constructorArgs = [true]
+  t.deepEqual(serialize(error).constructorArgs, [true])
+})
+
+test('constructorArgs can be non-enumerable', (t) => {
+  const error = new Error('test')
+  // eslint-disable-next-line fp/no-mutating-methods
+  Object.defineProperty(error, 'constructorArgs', {
+    value: [true],
+    enumerable: false,
+    writable: true,
+    configurable: true,
+  })
+  const newError = serialize(error)
+  t.deepEqual(newError.constructorArgs, [true])
+  t.true(
+    Object.getOwnPropertyDescriptor(newError, 'constructorArgs').enumerable,
+  )
+})
+
+const getArguments = function () {
+  // eslint-disable-next-line fp/no-arguments, prefer-rest-params
+  return arguments
+}
+
+test('constructorArgs can be "arguments"', (t) => {
+  const error = new Error('test')
+  error.constructorArgs = getArguments(true)
+  t.deepEqual(serialize(error).constructorArgs, [true])
+})
+
+// eslint-disable-next-line fp/no-class
+class CustomError extends Error {
+  constructor(...args) {
+    super(...args)
+    // eslint-disable-next-line fp/no-mutation, fp/no-this
+    this.args = args
+  }
+}
+
+const CUSTOM_ERROR_OBJECT = { ...SIMPLE_ERROR_OBJECT, name: CustomError.name }
+
+test('constructorArgs can be parsed', (t) => {
+  t.deepEqual(
+    parse(
+      { ...CUSTOM_ERROR_OBJECT, constructorArgs: [true] },
+      { classes: { CustomError } },
+    ).args,
+    [true],
+  )
+})
+
+test('constructorArgs that are not arrays are ignored', (t) => {
+  t.deepEqual(
+    parse(
+      { ...CUSTOM_ERROR_OBJECT, constructorArgs: true },
+      { classes: { CustomError } },
+    ).args,
+    [CUSTOM_ERROR_OBJECT.message, {}],
+  )
+})
+
+test('constructorArgs are removed during parsing', (t) => {
+  t.false(
+    'constructorArgs' in
+      parse(
+        { ...CUSTOM_ERROR_OBJECT, constructorArgs: [true] },
+        { classes: { CustomError } },
+      ),
+  )
+})
