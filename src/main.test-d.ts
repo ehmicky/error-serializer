fcd1b959ae509f9b0d53c3d64bde29feb2b0eeb6
ehmicky <ehmicky@gmail.com>
2022-09-25 13:40:56 +0200

    Improve type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -17,13 +17,14 @@ import {
 const error = new Error('test')
 
 expectAssignable<ErrorObject>(serialize(error))
-serialize(null)
-serialize({})
+expectType<null>(serialize(null))
+expectType<{}>(serialize({}))
 
 const name = 'Error'
 const message = 'test'
 const stack = 'testStack'
 expectAssignable<ErrorObject>({ name, message, stack })
+expectAssignable<ErrorObject>(serialize({ name, message, stack }))
 expectAssignable<ErrorObject>({
   name,
   message,
@@ -72,8 +73,9 @@ expectNotAssignable<ErrorObject>({ name, message, stack, prop: 0n })
 expectNotAssignable<ErrorObject>({ name, message, stack, prop: { one: 0n } })
 
 expectType<Error>(parse({}, { normalize: true }))
-parse(null)
-parse(error)
+expectType<null>(parse(null))
+expectType<{}>(parse({}))
+expectType<typeof error>(error)
 
 serialize({}, {})
 expectError(serialize({}, true))
@@ -81,21 +83,26 @@ expectAssignable<SerializeOptions>({})
 expectNotAssignable<SerializeOptions>({ unknown: true })
 expectAssignable<SerializeOptions>({ normalize: false })
 expectNotAssignable<SerializeOptions>({ normalize: 'true' })
+expectAssignable<SerializeOptions>({ shallow: false })
+expectNotAssignable<SerializeOptions>({ shallow: 'true' })
 
 parse({}, {})
 expectError(parse({}, true))
 expectAssignable<ParseOptions>({})
 expectNotAssignable<ParseOptions>({ unknown: true })
+
+expectAssignable<ParseOptions>({ shallow: false })
+expectNotAssignable<ParseOptions>({ shallow: 'true' })
+
 expectAssignable<ParseOptions>({ normalize: false })
 expectNotAssignable<ParseOptions>({ normalize: 'true' })
-
 expectType<true>(serialize(true))
 expectType<true>(serialize(true, { normalize: false }))
 expectType<ErrorObject>(serialize(true, { normalize: true }))
-
 expectType<true>(parse(true))
 expectType<true>(parse(true, { normalize: false }))
 expectType<Error>(parse(true, { normalize: true }))
+
 expectNotType<Error>(parse({ name: '', message: '' }))
 expectType<Error>(parse({ name: '', message: '', stack: '' }))
 
