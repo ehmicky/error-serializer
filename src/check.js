a23487a47b11c16c80809be16088856feb2fff49
ehmicky <ehmicky@gmail.com>
2022-09-25 13:26:59 +0200

    Fix comment

--- src/check.js
+++ src/check.js
@@ -1,17 +1,12 @@
 import isPlainObj from 'is-plain-obj'
 
 // Check if a value is an Error instance.
-// Only checks the top-level value, not `error.cause|errors`
-//  - If those are invalid, they should be normalized, even if `normalize` is
-//    `false`
 export const isErrorInstance = function (value) {
   return Object.prototype.toString.call(value) === '[object Error]'
 }
 
 // Check if a value conforms to the error plain object shape.
 // This enforces strict outputs.
-// Only checks the top-level value, not `error.cause|errors`, for the same
-// reason as above.
 export const isErrorObject = function (value) {
   return isPlainObj(value) && hasCoreProps(value)
 }
