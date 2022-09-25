58a4f3a5c0c83868fef3a211542755eb4664ab45
ehmicky <ehmicky@gmail.com>
2022-09-25 13:26:56 +0200

    Move lines

--- test/serialize.js
+++ test/serialize.js
@@ -64,3 +64,21 @@ test('Can serialize and parse deeply', (t) => {
   t.deepEqual(error, FULL_ERROR)
   t.true(item)
 })
+
+test('Remove unsafe non-core properties when serializing', (t) => {
+  const error = new Error('test')
+  error.prop = 0n
+  t.is(serialize(error).prop, undefined)
+})
+
+test('Remove circular non-core properties when serializing', (t) => {
+  const error = new Error('test')
+  t.is(serialize(error).prop, undefined)
+})
+
+test('Make non-core properties JSON-safe when serializing', (t) => {
+  const error = new Error('test')
+  const date = new Date()
+  error.prop = [Number.NaN, undefined, true, date]
+  t.deepEqual(serialize(error).prop, [true, date.toJSON()])
+})
